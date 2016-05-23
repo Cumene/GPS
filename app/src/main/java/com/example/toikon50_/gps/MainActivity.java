1051bcb4b6ba17ac24fd2afda46b8047ce05824e
Cumene <toikon43@gmail.com>
2016-05-23 14:56:19 +0900

    Get location.

---
+++ app/src/main/java/com/example/toikon50_/gps/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.toikon50_.gps;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity implements LocationListener {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // LocationManagerを取得
+        LocationManager mLocationManager =
+                (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Criteriaオブジェクトを生成
+        Criteria criteria = new Criteria();
+
+        // Accuracyを指定(低精度)
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+
+        // PowerRequirementを指定(低消費電力)
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+
+        // ロケーションプロバイダの取得
+        String provider = mLocationManager.getBestProvider(criteria, true);
+
+        // 取得したロケーションプロバイダを表示
+        TextView tv_provider = (TextView) findViewById(R.id.Provider);
+        tv_provider.setText("Provider: " + provider);
+
+        // LocationListenerを登録
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocationManager.requestLocationUpdates(provider, 0, 0, this);
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // 緯度の表示
+        TextView tv_lat = (TextView) findViewById(R.id.Latitude);
+        tv_lat.setText("Latitude:"+location.getLatitude());
+
+        // 経度の表示
+        TextView tv_lng = (TextView) findViewById(R.id.Longitude);
+        tv_lng.setText("Latitude:"+location.getLongitude());
+
+        //高度
+        TextView tv_ati = (TextView) findViewById(R.id.Altitude);
+        tv_ati.setText("Altitude:"+location.getAltitude());
+
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+}
